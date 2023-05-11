8ee6e693499195ac4f749dee8df9028387b832ca
120821 <1208215065@qq.com>
2023-05-11 17:39:08 +0800

    测试，图表，但是本地的mysql不支持，待解决

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Chart
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.active_record.default_timezone = :utc
+    config.time_zone = 'Etc/UTC'
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- db/migrate/20230511091006_create_orders.rb
+++ db/migrate/20230511091006_create_orders.rb
@@ -6,5 +6,19 @@ class CreateOrders < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
+    5.times do |i|
+     order = Order.create(price: 100 * i, status: 'dispatched')
+     order.update(created_at: order.created_at - i.day)
+    end
+
+    5.times do |i|
+     order = Order.create(price: 100 * i, status: 'pending')
+     order.update(created_at: order.created_at - i.day)
+    end
+
+    5.times do |i|
+     order = Order.create(price: 100 * i, status: 'delivered')
+     order.update(created_at: order.created_at - i.day)
+    end
   end
 end
